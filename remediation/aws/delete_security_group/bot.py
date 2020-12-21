9a27401f65533b6adf69ea378187937fc08e13d2
joshua laver <joshua.laver@sonraisecurity.com>
2020-12-21 13:02:23 -0400

    feat: fixes

--- remediation/aws/delete_security_group/bot.py
+++ remediation/aws/delete_security_group/bot.py
@@ -7,12 +7,7 @@ def run(ctx):
     ec2_client = ctx.get_client().get('ec2')
 
     # Parse resource id to group
-    resource_id = ctx.resource_id
-    resource_arn = sonrai.platform.aws.arn.parse(resource_id)
-    group_id = resource_arn \
-        .assert_service("ec2") \
-        .assert_type("securitygroup") \
-        .name
+    group_id = ctx.resource_id
 
     # Refers doc https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html?highlight=delete%20security%20group#EC2.Client.delete_security_group
     # Refers doc https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DeleteSecurityGroup.html
